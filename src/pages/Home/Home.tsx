3eca78a080cadacb19baa042e4a1bb531c0d59ec
Lê Văn Tuân <tuan.lv@agiletechsoftware.com>
2024-04-23 11:32:38 +0700

    chore: initial

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <button type="button" className="btn btn-primary">
+        Base class
+      </button>
+
+      <h1>Accelerate the world’s research one reference at a time.</h1>
+    </div>
+  );
+};
+
+export default HomePage;
